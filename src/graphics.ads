471612f2cdf6fdf94b5ce922b29b0b28bc5b2e87
Skye Stout <srs6513@psu.edu>
2025-11-23 17:26:51 -0500

    Integrate Buffer_T into common Graphics package, rename some methods for clarity, move away from storing pixel arrays via access types, removed Buffer_T destructor, removed unneeded types from original .ads

--- src/graphics.ads
+++ src/graphics.ads
@@ -18,4 +18,37 @@ package graphics is
       Is_Bold          : Boolean     := False;
    end record;
 
+   --  Maximum resolution for the display (Change later)
+   type TUI_Width is new Integer range 1 .. 80;
+   type TUI_Height is new Integer range 1 .. 50;
+
+   --  Defines two-dimensional array of type Pixel that holds the pixels
+   type Pixel_Array is array (TUI_Width, TUI_Height) of Pixel;
+   type Pixel_Array_Ptr is access Pixel_Array;
+
+   -- Buffer_T is a record that holds the pixel data and its size
+   type Buffer_T is record
+      Width : TUI_Width := TUI_Width'Last;
+      Height : TUI_Height := TUI_Height'Last;
+      --  No default value needed, because Pixel already has defaults
+      Data : aliased Pixel_Array;
+   end record;
+
+   --  Constructor to create and initialize buffer instance
+   function Create_Buffer (Width : in TUI_Width;
+                           Height: in TUI_Height)
+                           return Buffer_T;
+
+   --  Mutator - Changes state of Pixel
+   procedure Set_Buffer_Pixel (B : in out Buffer_T;
+                               X : in TUI_Width;
+                               Y : in TUI_Height;
+                               P : in Pixel);
+
+   --  Accessor - Observes state of Pixel
+   function Get_Buffer_Pixel (B : in Buffer_T;
+                              X : in TUI_Width;
+                              Y : in TUI_Height)
+                              return Pixel;
+
 end graphics;
