5fde12b76aa4923497ea0b9495ce5199a8be5a26
Skye Stout <srs6513@psu.edu>
2025-11-30 21:33:13 -0500

    Add custom system to cycle text color

--- demos/src/user_library.ads
+++ demos/src/user_library.ads
@@ -1,9 +1,12 @@
 with Components;
+with ECS;
 
 package User_Library is
 
    type Custom_Component is new Components.Component_T with record
       Hue_Change_Speed : Integer := 0;
    end record;
+
+   procedure Custom_System (Entity_List : ECS.Entity_Components);
 
 end User_Library;
