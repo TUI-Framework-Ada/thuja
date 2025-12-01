0a4b9c8ee6e2cc63011a29f046db51ba5c3a98b1
Skye Stout <srs6513@psu.edu>
2025-11-30 20:22:09 -0500

    Whoops, forgot to qualify

--- demos/src/user_library.ads
+++ demos/src/user_library.ads
@@ -2,7 +2,7 @@ with Components;
 
 package User_Library is
 
-   type Custom_Component is new Component_T with record
+   type Custom_Component is new Components.Component_T with record
       Hue_Change_Speed : Integer := 0;
    end record;
 
