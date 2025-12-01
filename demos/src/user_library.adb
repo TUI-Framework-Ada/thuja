378da7c945bfe73d7c9fa5591f9c374b1748631d
Skye Stout <srs6513@psu.edu>
2025-11-30 21:33:13 -0500

    Add custom system to cycle text color

---
+++ demos/src/user_library.adb
@@ -0,0 +1,66 @@
+with Ada.Calendar;
+with Graphics;
+with IDs;
+
+package body User_Library is
+
+   procedure Custom_System (Entity_List : ECS.Entity_Components) is
+      Search_Component_IDs : IDs.Component_ID_Vector.Vector;
+      Matched_Entities : IDs.Entity_ID_Vector.Vector;
+      Component_List : ECS.Components_Ptr;
+      Text_C : Components.Text_Component_T;
+      Custom_C : Custom_Component;
+      Now : Ada.Calendar.Time := Ada.Calendar.Clock;
+      Unused1 : Ada.Calendar.Year_Number;
+      Unused2 : Ada.Calendar.Month_Number;
+      Unused3 : Ada.Calendar.Day_Number;
+      Seconds : Ada.Calendar.Day_Duration;
+      Hue : Integer;
+      X : Float;
+      Rp : Float;
+      Gp : Float;
+      Bp : Float;
+   begin
+      Search_Component_IDs.Append (IDs.To_CID ("TextComponent"));
+      Search_Component_IDs.Append (IDs.To_CID ("Custom_Component"));
+      Matched_Entities := ECS.Get_Entities_Matching (Entity_List, Search_Component_IDs);
+      for EID of Matched_Entities loop
+         Component_List := ECS.Get_Entity_Components (Entity_List, EID);
+         Text_C := Components.Text_Component_T (
+            ECS.Get_Component (Component_List.all, IDs.To_CID ("TextComponent"))
+                                               );
+         Custom_C := Custom_Component (
+            ECS.Get_Component (Component_List.all, IDs.To_CID ("Custom_Component"))
+                                      );
+
+         --  Update text color based on current time
+         Ada.Calendar.Split (Now, Unused1, Unused2, Unused3, Seconds);
+         Hue := (Integer (Seconds) * Custom_C.Hue_Change_Speed) mod 360;
+         X := 1.0 - abs (Float'Remainder (Float (Hue) / 60.0, 2.0) - 1.0);
+         if Hue < 60 then
+               Rp := 1.0; Gp := X; Bp := 0.0;
+         elsif Hue < 120 then
+               Rp := X; Gp := 1.0; Bp := 0.0;
+         elsif Hue < 180 then
+               Rp := 0.0; Gp := 1.0; Bp := X;
+         elsif Hue < 240 then
+               Rp := 0.0; Gp := X; Bp := 1.0;
+         elsif Hue < 300 then
+               Rp := X; Gp := 0.0; Bp := 1.0;
+         elsif Hue <= 360 then
+               Rp := 1.0; Gp := 0.0; Bp := X;
+         end if;
+         Text_C.Text_Color.Red := Graphics.u8 (Rp * 255.0);
+         Text_C.Text_Color.Green := Graphics.u8 (Gp * 255.0);
+         Text_C.Text_Color.Blue := Graphics.u8 (Bp * 255.0);
+
+         --  Update text component
+         ECS.Add_Component (
+            ECS.Get_Entity_Components (Entity_List, EID).all,
+            IDs.To_CID ("TextComponent"),
+            Text_C
+                           );
+      end loop;
+   end Custom_System;
+
+end User_Library;
